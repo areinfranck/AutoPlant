cf11ff4abf0f38e2e62adadc826130b9ebce3101
TheMindDroid <TheMindDroid@gmail.com>
2020-08-09 22:45:48 -0700

    comitting files for the first time

---
+++ src/main/java/me/theminddroid/autoplant/AutoPlant.java
@@ -0,0 +1,53 @@
+package me.theminddroid.autoplant;
+
+import com.sk89q.worldguard.WorldGuard;
+import com.sk89q.worldguard.protection.flags.Flag;
+import com.sk89q.worldguard.protection.flags.StateFlag;
+import com.sk89q.worldguard.protection.flags.registry.FlagConflictException;
+import com.sk89q.worldguard.protection.flags.registry.FlagRegistry;
+import me.theminddroid.autoplant.events.Crops;
+import org.bukkit.Bukkit;
+import org.bukkit.plugin.Plugin;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class AutoPlant extends JavaPlugin {
+
+    public static StateFlag AUTO_PLANT;
+
+    @Override
+    public void onEnable() {
+        System.out.println("The plugin has started...");
+        getServer().getPluginManager().registerEvents(new Crops(), this);
+    }
+
+    @Override
+    public void onLoad() {
+        FlagRegistry registry = WorldGuard.getInstance().getFlagRegistry();
+        try {
+            StateFlag flag = new StateFlag("auto-plant", false);
+            registry.register(flag);
+            AUTO_PLANT = flag;
+        } catch (FlagConflictException e) {
+            Flag<?> existing = registry.get("auto-plant");
+            if (existing instanceof StateFlag) {
+                AUTO_PLANT = (StateFlag) existing;
+            } else {
+                getLogger().severe("Unable to load flag.");
+                throw e;
+            }
+        }
+    }
+
+    public static AutoPlant getInstance() {
+        Plugin plugin = Bukkit.getServer().getPluginManager().getPlugin("AutoPlant");
+        if (!(plugin instanceof AutoPlant)) {
+            throw new RuntimeException("'AutoPlant' not found. 'AutoPlant' plugin disabled?");
+        }
+        return ((AutoPlant) plugin);
+    }
+
+    @Override
+    public void onDisable() {
+        System.out.println("The plugin is shutting down...");
+    }
+}
---
+++ src/main/java/me/theminddroid/autoplant/events/Crops.java
@@ -0,0 +1,153 @@
+package me.theminddroid.autoplant.events;
+
+import com.google.common.collect.ImmutableMap;
+import com.sk89q.worldedit.bukkit.BukkitAdapter;
+import com.sk89q.worldedit.world.World;
+import com.sk89q.worldguard.LocalPlayer;
+import com.sk89q.worldguard.WorldGuard;
+import com.sk89q.worldguard.bukkit.WorldGuardPlugin;
+import com.sk89q.worldguard.protection.ApplicableRegionSet;
+import com.sk89q.worldguard.protection.managers.RegionManager;
+import com.sk89q.worldguard.protection.regions.RegionContainer;
+import me.theminddroid.autoplant.AutoPlant;
+import org.bukkit.Bukkit;
+import org.bukkit.Material;
+import org.bukkit.block.Block;
+import org.bukkit.block.data.Ageable;
+import org.bukkit.block.data.BlockData;
+import org.bukkit.block.data.Directional;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.block.BlockBreakEvent;
+
+import java.util.EnumSet;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+public class Crops implements Listener {
+
+    Set<Material> cropList = EnumSet.of(Material.WHEAT, Material.POTATOES, Material.CARROTS, Material.COCOA, Material.BEETROOTS, Material.NETHER_WART);
+    Map<Material, Material> treeMaterials = new ImmutableMap.Builder<Material, Material>()
+            .put(Material.OAK_LOG, Material.OAK_SAPLING)
+            .put(Material.BIRCH_LOG, Material.BIRCH_SAPLING)
+            .put(Material.SPRUCE_LOG, Material.SPRUCE_SAPLING)
+            .put(Material.ACACIA_LOG, Material.ACACIA_SAPLING)
+            .put(Material.DARK_OAK_LOG, Material.DARK_OAK_SAPLING)
+            .put(Material.JUNGLE_LOG, Material.JUNGLE_SAPLING)
+            .put(Material.CRIMSON_STEM, Material.CRIMSON_FUNGUS)
+            .put(Material.WARPED_STEM, Material.WARPED_FUNGUS)
+            .put(Material.STRIPPED_BIRCH_LOG, Material.BIRCH_SAPLING)
+            .put(Material.STRIPPED_OAK_LOG, Material.OAK_SAPLING)
+            .put(Material.STRIPPED_SPRUCE_LOG, Material.SPRUCE_SAPLING)
+            .put(Material.STRIPPED_ACACIA_LOG, Material.ACACIA_SAPLING)
+            .put(Material.STRIPPED_DARK_OAK_LOG, Material.DARK_OAK_SAPLING)
+            .put(Material.STRIPPED_JUNGLE_LOG, Material.JUNGLE_SAPLING)
+            .put(Material.STRIPPED_CRIMSON_STEM, Material.CRIMSON_FUNGUS)
+            .put(Material.STRIPPED_WARPED_STEM, Material.WARPED_FUNGUS)
+            .build();
+    Set<Material> saplings = new HashSet<>(treeMaterials.values());
+
+    @EventHandler
+    public void cropBroken(BlockBreakEvent event) {
+
+        Player player = event.getPlayer();
+        Block block = event.getBlock();
+
+        final com.sk89q.worldedit.util.Location worldGuardLocation = BukkitAdapter.adapt(block.getLocation());
+        RegionContainer container = WorldGuard.getInstance().getPlatform().getRegionContainer();
+        RegionManager regions = container.get((World) worldGuardLocation.getExtent());
+
+        if (regions == null) {
+            Bukkit.getLogger().finer("WorldGuard failed to return region manager for world.");
+            return;
+        }
+
+        ApplicableRegionSet set = regions.getApplicableRegions(worldGuardLocation.toVector().toBlockPoint());
+        LocalPlayer localPlayer = WorldGuardPlugin.inst().wrapPlayer(player);
+
+        if (!set.testState(localPlayer, AutoPlant.AUTO_PLANT)) {
+            Bukkit.getLogger().finer("Player not in autoplant region.");
+            return;
+        }
+
+        if (player.hasPermission("autoplant.bypass")) {
+            Bukkit.getLogger().finer("Player has autoplant bypass.");
+            return;
+        }
+
+        Bukkit.getLogger().finer("Crops triggered");
+
+        handleCrop(event, player, block);
+        handleTree(block);
+        handleSapling(event, player, block);
+    }
+
+    private void handleSapling(BlockBreakEvent event, Player player, Block block) {
+        Material material = block.getType();
+
+        if (saplings.contains(material)) {
+            event.setCancelled(true);
+            player.sendTitle("","§2That tree isn't fully grown yet!", 10, 30,10);
+        }
+    }
+
+
+    private void handleCrop(BlockBreakEvent event, Player player, Block block) {
+        BlockData blockData = block.getBlockData();
+        Material material = block.getType();
+
+        if (!(blockData instanceof Ageable)) {
+            return;
+        }
+
+        if (!cropList.contains(material)) {
+            return;
+        }
+
+        Bukkit.getLogger().finer("Handling crop.");
+
+        Ageable age = (Ageable) blockData;
+
+        if (age.getAge() != age.getMaximumAge()) {
+
+            event.setCancelled(true);
+            player.sendTitle("","§2That crop isn't fully grown yet!", 10, 30,10);
+            return;
+        }
+
+        Bukkit.getScheduler().runTaskLater(AutoPlant.getInstance(), () -> {
+            block.setType(material);
+
+            BlockData newBlockData = block.getBlockData();
+            if (blockData instanceof Directional && newBlockData instanceof Directional) {
+                Directional newDirectional = (Directional) newBlockData;
+                Directional oldDirectional = (Directional) blockData;
+
+                newDirectional.setFacing(oldDirectional.getFacing());
+            }
+            block.setBlockData(newBlockData);
+        }, 1);
+    }
+
+
+    private void handleTree(Block block) {
+        Material material = block.getType();
+
+        if (!(treeMaterials.containsKey(material))) {
+            Bukkit.getLogger().finer("Tree not found " + material);
+            return;
+        }
+
+        Bukkit.getLogger().finer("Handling tree.");
+
+        if (!(block.getLocation().subtract(0.0, 1.0, 0.0).getBlock().getType().equals(Material.DIRT))
+            && !(block.getLocation().subtract(0.0, 1.0, 0.0).getBlock().getType().equals(Material.WARPED_NYLIUM))
+            && !(block.getLocation().subtract(0.0, 1.0, 0.0).getBlock().getType().equals(Material.CRIMSON_NYLIUM))) {
+            Bukkit.getLogger().finer("Log not on dirt.");
+            return;
+        }
+        Bukkit.getScheduler().runTaskLater(AutoPlant.getInstance(), () -> block.setType(treeMaterials.get(material)), 1);
+    }
+}
